b35c879ec3278a039c8f8f1aa65e73b213770d87
paluan-batista <paluan.silva@picpay.com>
2022-10-18 16:34:45 -0300

    - add server

---
+++ server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
+
+var startTime time.Time = time.Now()
+
+func server() {
+	e := gin.Default()
+	e.GET("/ping", func(ctx *gin.Context) {
+		if time.Since(startTime) < 5*time.Second {
+			ctx.String(http.StatusInternalServerError, "pong")
+			return
+		}
+		ctx.String(http.StatusOK, "pong")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	e.Run(":8080")
+}
